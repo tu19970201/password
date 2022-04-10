be740c9122dcb48ff527e403c3d8d134ec9e98cb
Alan Tu <tu19970201@gmail.com>
2022-04-10 13:56:40 +0800

    teacher version

--- password.py
+++ password.py
@@ -1,13 +1,17 @@
 chance = 3
+real_password = 'a123456'
 
 while chance > 0:
     password = input('請輸入密碼')
+    chance = chance - 1
     
-    while password != 'a123456':
-        chance = chance - 1
-        print('密碼錯誤，還有', chance , '次機會')
+    if password == real_password:
+        print('登入成功')
         break
 
-    if password == 'a123456':
-        print('登入成功')
-        chance = 0
+    else:
+        print('密碼錯誤')
+        if chance > 0:
+            print('還有', chance , '次機會')
+        else:
+            print('您的帳號已被鎖定')
